2b9b95726eb60d4591f3968e9ce785ed23e67247
ibrahim-good-abilities <ibrahim.eldesoki@goodabilities.com>
2020-02-12 00:03:29 +0200

    add Apis for fb ,tw,google , facebook

--- config/services.php
+++ config/services.php
@@ -29,5 +29,26 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'facebook' => [
+        'client_id' => '489849808385029', //Facebook API
+        'client_secret' => 'ca8c44ac700761c5566cbad24e578abc', //Facebook Secret
+        'redirect' => 'https://demo.corinthi.com/login/facebook/callback',
+    ],
+    'google' => [
+         'client_id' =>'1055268790332-uk0v8tpke3i9db0qbcbm6d2bl2k288al.apps.googleusercontent.com',
+         'client_secret' =>'XvKr9y6ee_aoeDpQQBxR28sf',
+         'redirect' => 'https://demo.corinthi.com/login/google/callback'
+    ],
+     'twitter' => [
+                'client_id' =>'8MwH9P9kfyvZKhhRvc3OFurXH',
+                'client_secret' =>'h01lU5FYr1qeIyQcCnroCH1eR5apXLkOqkWZPF19VllCr8y8Jb',
+                'redirect' => 'https://demo.corinthi.com/login/twitter/callback'
+     ],
+    'linkedin' => [
+            'client_id' =>'77nrivo9cyagi8',
+            'client_secret' =>'Nb7XcPzdfz8stJXc',
+            'redirect' => 'https://demo.corinthi.com/login/linkedin/callback'
+    ]
+
 
 ];
